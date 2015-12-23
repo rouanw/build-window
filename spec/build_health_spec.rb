cf2fc52ab2d54502ce07037de1cdf84c4e773af0
Rouan <rmw@thoughtworks.com>
2015-12-23 20:43:02 +0200

    unit tests for bamboo build health

--- spec/build_health_spec.rb
+++ spec/build_health_spec.rb
@@ -18,7 +18,7 @@ describe 'get build health' do
             "started_at" => 4444444
           }
         ]
-        stub_request(:get, "https://api.travis-ci.org/repos/myrepo/mybuild/builds?event_type=push").
+        stub_request(:get, 'https://api.travis-ci.org/repos/myrepo/mybuild/builds?event_type=push').
              to_return(:status => 200, :body => @travis_response.to_json, :headers => {})
       end
 
@@ -62,9 +62,101 @@ describe 'get build health' do
 
       it 'should return the build health' do
         so_so = [{"result" => 1}, {"result" => 0}]
-        stub_request(:get, "https://api.travis-ci.org/repos/myrepo/mybuild/builds?event_type=push").
+        stub_request(:get, 'https://api.travis-ci.org/repos/myrepo/mybuild/builds?event_type=push').
              to_return(:status => 200, :body => so_so.to_json, :headers => {})
         build_health = get_build_health 'id' => 'myrepo/mybuild', 'server' => 'Travis'
+        expect(build_health[:health]).to eq(50)
+      end
+  end
+
+  describe 'for bamboo build' do
+      module Builds
+        BUILD_CONFIG = {"bambooBaseUrl"=> 'http://bamboo-place'}
+      end
+
+      before(:each) do
+        @bamboo_response = {
+          "results"  => {
+            "result" => [
+              {
+                "state" => "Successful",
+                "buildDurationDescription" => "some time it took",
+                "plan" => {"shortName" => "Harvey"},
+                "key" => 33,
+                "buildRelativeTime" => "a long long time ago"
+              }
+            ]
+          }
+        }
+        stub_request(:get, 'http://bamboo-place/rest/api/latest/result/MY-BUILD.json?expand=results.result').
+             to_return(:status => 200, :body => @bamboo_response.to_json, :headers => {})
+      end
+
+      it 'should get travis build info from travis api' do
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
+        expect(WebMock.a_request(:get, 'http://bamboo-place/rest/api/latest/result/MY-BUILD.json?expand=results.result')).to have_been_made
+      end
+
+      it 'should return the name of the build' do
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
+        expect(build_health[:name]).to eq('Harvey')
+      end
+
+      it 'should return the status of the latest build when Successful' do
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
+        expect(build_health[:status]).to eq('Successful')
+      end
+
+      it 'should return the status of the latest build when Failed' do
+        failed_build = {
+          "results"  => {
+            "result" => [
+              {
+                "state" => "Failed",
+                "plan" => {},
+              }
+            ]
+          }
+        }
+        stub_request(:get, 'http://bamboo-place/rest/api/latest/result/MY-BUILD.json?expand=results.result').
+             to_return(:status => 200, :body => failed_build.to_json, :headers => {})
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
+        expect(build_health[:status]).to eq('Failed')
+      end
+
+      it 'should return the duration of the latest build' do
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
+        expect(build_health[:duration]).to eq('some time it took')
+      end
+
+      it 'should return a link to the latest build' do
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
+        expect(build_health[:link]).to eq('http://bamboo-place/browse/33')
+      end
+
+      it 'should return the time of the latest build' do
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
+        expect(build_health[:time]).to eq('a long long time ago')
+      end
+
+      it 'should return the build health' do
+        so_so = {
+          "results"  => {
+            "result" => [
+              {
+                "state" => "Failed",
+                "plan" => {},
+              },
+              {
+                "state" => "Successful",
+                "plan" => {},
+              }
+            ]
+          }
+        }
+        stub_request(:get, 'http://bamboo-place/rest/api/latest/result/MY-BUILD.json?expand=results.result').
+             to_return(:status => 200, :body => so_so.to_json, :headers => {})
+        build_health = get_build_health 'id' => 'MY-BUILD', 'server' => 'Bamboo'
         expect(build_health[:health]).to eq(50)
       end
   end
